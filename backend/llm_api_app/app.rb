a813e155e3d36a6d7ae836b2cb01c22389cedf42
John Rothfield <johnrothfield@gmail.com>
2025-07-07 07:18:17 -0600

    initial

--- backend/llm_api_app/app.rb
+++ backend/llm_api_app/app.rb
@@ -1,2 +1,29 @@
-safe-paste backend/llm_api_app/app.rb "Serve static index.html from public folder in Sinatra (from chat)"
+require 'sinatra'
+require 'json'
+require 'httparty'
 
+LLM_API_URL = "https://api.your-llm-provider.com/v1/generate"
+LLM_API_KEY = ENV['LLM_API_KEY']
+
+def load_prompt(filename)
+  File.read(File.join('prompts', filename))
+end
+
+def call_llm_api(prompt)
+  HTTParty.post(LLM_API_URL,
+    headers: {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer #{LLM_API_KEY}"
+    },
+    body: { prompt: prompt, max_tokens: 1000 }.to_json
+  )
+end
+
+post '/generate' do
+  content_type :json
+  request_payload = JSON.parse(request.body.read)
+  prompt_file = request_payload["prompt_file"]
+  prompt = load_prompt(prompt_file)
+  response = call_llm_api(prompt)
+  { result: response.parsed_response }.to_json
+end
